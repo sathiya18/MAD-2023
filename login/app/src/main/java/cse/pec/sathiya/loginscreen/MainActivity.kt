7d768da2bad593e5027d141e3150327ed53869aa
Sathiyamurthy <sathiyamurthyk@ptuniv.edu.in>
2024-09-28 10:53:32 +0530

    login

---
+++ login/app/src/main/java/cse/pec/sathiya/loginscreen/MainActivity.kt
@@ -0,0 +1,13 @@
+package cse.pec.sathiya.loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.frame_layout_example_two)
+        setContentView(R.layout.login_screen)
+
+    }
+}
